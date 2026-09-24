72c21003c244e8ed2a668a82bb8519d5bef72da4
Michel Fodje <michel4j@gmail.com>
2026-09-23 22:34:46 -0600

    fix(pdb-detail): update image URL template and improve layout for PDB detail view

--- mxlive/settings.py
+++ mxlive/settings.py
@@ -137,7 +137,7 @@ AUTHENTICATION_BACKENDS = [
 SECURE_REFERRER_POLICY = "strict-origin-when-cross-origin"
 SECURE_CSP = {
     "default-src": [CSP.SELF],
-    "img-src": ["data:", CSP.SELF],
+    "img-src": ["data:", CSP.SELF, "https://cdn.rcsb.org"],
     "style-src": [CSP.SELF, CSP.UNSAFE_INLINE, "https://fonts.googleapis.com"],
     "font-src": [CSP.SELF, "https://fonts.gstatic.com"],
     "script-src": [CSP.UNSAFE_INLINE, CSP.UNSAFE_EVAL, CSP.SELF],
